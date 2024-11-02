077c60783d1ae31e122c7013a329c8a6a3fdab23
Paulo Ribeiro <paribe@gmail.com>
2024-11-02 18:18:10 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import psycopg2
 # Configuração da conexão com o PostgreSQL
 def get_connection():
     return psycopg2.connect(
-        host="csj85ra3esus7381vvd0-a.oregon-postgres.render.com",
+        host="",
         database="database_data_vault",
         user="database_data_vault_user",
-        password="KoInF5F0MC3dpUZ8F1DhNKuOTLhYBFYy"
+        password=""
     )
